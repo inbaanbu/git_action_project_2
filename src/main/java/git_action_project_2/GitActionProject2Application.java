af75146ea85dbc7031cc7fafcd86ad04c90156aa
inbaanbu <inbarasan19981230@gmail.com>
2024-01-21 12:58:13 +0530

    first commit

---
+++ src/main/java/git_action_project_2/GitActionProject2Application.java
@@ -0,0 +1,13 @@
+package git_action_project_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/git_action_project_2/GitActionProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package git_action_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
