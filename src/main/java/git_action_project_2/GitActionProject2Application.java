c47f5f3dd42941ec6679519e39530dfa2fadd87e
inbaanbu <inbarasan19981230@gmail.com>
2024-01-23 00:03:17 +0530

    bug fix

--- src/main/java/git_action_project_2/GitActionProject2Application.java
+++ src/main/java/git_action_project_2/GitActionProject2Application.java
@@ -4,16 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @SpringBootApplication
 public class GitActionProject2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionProject2Application.class, args);
 	}
+	
+	@GetMapping("/")
+	public String user() {
+		return "hii inba code is working";
+	}
+	
+
 
 	@GetMapping("/user/details")
-	public String user() {
+	public String user1() {
 		return "hii inba code is working";
 	}
 
