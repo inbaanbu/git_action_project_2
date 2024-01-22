1ec39fca1208cf6c8ebcbb9eabfcaced5b5f2733
inbaanbu <138519340+inbaanbu@users.noreply.github.com>
2024-01-22 23:38:02 +0530

    Update GitActionProject2Application.java

--- src/main/java/git_action_project_2/GitActionProject2Application.java
+++ src/main/java/git_action_project_2/GitActionProject2Application.java
@@ -2,12 +2,19 @@ package git_action_project_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class GitActionProject2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitActionProject2Application.class, args);
+	}
+
+	@GetMapping("/user/details")
+	public String user() {
+		return "hii inba code is working";
 	}
 
 }
